7a06d3bb5b66a8fe96c88e9dd8d490d092896e6f
Taideptrai <tai47406@donga.edu.vn>
2021-05-13 12:46:51 +0700

    Js day 3

---
+++ index.js
@@ -0,0 +1,38 @@
+const soTienVayInput = document.getElementById('money')
+const thoiGianVayInput = document.getElementById('time')
+const laiSuatInput = document.getElementById('rate')
+const calButton = document.getElementById('cal')
+
+var soTienVay = 0;
+var thoiGianVay = 0;
+var laiSuat = 0;
+
+
+soTienVayInput.addEventListener('change',(e)=>{
+    soTienVay = e.target.value;
+    console.log(soTienVay)
+})
+
+
+thoiGianVayInput.addEventListener('change',(e)=>{
+    thoiGianVay = e.target.value;
+    console.log(thoiGianVay)
+})
+
+laiSuatInput.addEventListener('change',(e)=>{
+    laiSuat = e.target.value;
+    console.log(laiSuat)
+})
+
+calButton.addEventListener('click',(e) =>{
+var gocHangThang = soTienVay/thoiGianVay;
+for(let i = 0 ; i < thoiGianVay ; i++) {
+  var  laiPhaiTra = soTienVay * laiSuat/12;
+    var tienPhaiTraHangThang = laiPhaiTra +  gocHangThang;
+    var gocConLai = soTienVay - gocHangThang;
+
+    console.log('Tien goc:' + gocConLai + ' Goc:' + gocHangThang + 'Lai:' + laiPhaiTra + ' Goc + Lai: ' + tienPhaiTraHangThang);
+
+  }
+} )
+
