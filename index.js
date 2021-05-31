87d6eab98de66cb4eadcfdada4b682882db94359
Taideptrai <tai47406@donga.edu.vn>
2021-05-31 22:55:37 +0700

    co gang hoan thien Assignment 2

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const soTienVayInput = document.getElementById('money')
 const thoiGianVayInput = document.getElementById('time')
 const laiSuatInput = document.getElementById('rate')
 const calButton = document.getElementById('cal')
+const inputNgayGiaiNgan = document.getElementById('date')
 
 var soTienVay = 0;
 var thoiGianVay = 0;
 var laiSuat = 0;
-
+var ngayGiaiNgan = 0
+var date;
 
 soTienVayInput.addEventListener('change',(e)=>{
     soTienVay = e.target.value;
@@ -24,15 +26,25 @@ laiSuatInput.addEventListener('change',(e)=>{
     laiSuat= laiSuat/100;
     console.log(laiSuat)
 })
+inputNgayGiaiNgan.addEventListener('change',(e)=>{
+    ngayGiaiNgan = e.target.value;
+    date = new Date(e.target.value);
+})
 
 calButton.addEventListener('click',(e) =>{
-var gocHangThang = soTienVay/thoiGianVay;
-for(let i = 0 ; i <=  thoiGianVay ; i++) {
-  var  laiPhaiTra = soTienVay * laiSuat/12;
-    let gocConLai = soTienVay - gocHangThang;
-    let tienPhaiTraHangThang = laiPhaiTra +  gocHangThang;
-    console.log('Tien goc:' + gocConLai + ' Goc:' + gocHangThang + 'Lai:' + laiPhaiTra + ' Goc + Lai: ' + tienPhaiTraHangThang);
-    // gán lại giá trị ở chỗ này đúng k?
-  }
+    var laiPhaiTra = 0;
+    var gocConLai = Math.round(soTienVay);
+    var tienPhaiTraHangThang = 0;
+    var gocHangThang = Math.round(soTienVay/thoiGianVay);
+
+
+    for (let z = 1; z <= thoiGianVay; z++) {
+        laiPhaiTra = Math.round(gocConLai * laiSuat/thoiGianVay);
+        tienPhaiTraHangThang = Math.round(laiPhaiTra + gocHangThang);
+        gocConLai = Math.round(gocConLai - gocHangThang);
+        console.log("Số gốc còn lại = " + gocConLai + " Gốc hàng tháng = " + gocHangThang +" Lãi phải trả = " + laiPhaiTra + ' Tổng gốc + lãi = '+ tienPhaiTraHangThang);
+
+
+      }
 } )
 
